6f7874fd77f25d6a464b4944444829e7e26dd833
João Vitor <joao_vitorsoares@hotmail.com>
2025-11-06 16:42:06 -0300

    mensagem mudança

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <div style={{backgroundColor: "#eee", color: "#202020ff"}}>Vir Rezende, você esta vendo essa mensagem ?</div>
+    <div style={{backgroundColor: "#eee", color: "#202020ff"}}>Alô Vir Rezende, você esta vendo essa mensagem ? </div>
   )
 }
 
